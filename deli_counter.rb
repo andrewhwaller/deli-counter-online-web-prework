631c405cbf0ac17432b4edc567aff4ce1e76e58c
Andrew Waller <andrewhwaller@gmail.com>
2019-03-17 04:37:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,15 @@
 katz_deli = []
 
-def line(katz_deli)
+def line(deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
-  else puts "The line is currently: (katz_deli)"
+  else 
+    current_line = "The line is currently:"
+    deli.each_with_index { |name, index| "#{index + 1}. #{name}" }
   end
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
+def take_a_number(deli, name)
+  deli.push(name)
+  puts "Welcome, #{name}. You are number #{deli.index(name) + 1} in line."
 end
